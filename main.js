916bcad4a1070b4dc328036889d533cfa1b27dee
Mayra Rodriguez <tustinoffices@gmail.com>
2016-05-02 22:08:44 -0700

    cards flip

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 /**
  * Created by Mayra on 4/26/2016.
  */
+var firstCardClicked = null;
+var secondCardClicked = null;
+var totalPossibleMatches = 10;
+var matchCounter = 0;
+
+$(document).ready(function(){
+    $('.card').click(function(){
+        //console.log('document ready function:', this);
+        flipCard(this);
+    });
+});
+function flipCard(cardClicked){
+    //console.log("flip card function this:", this);
+    //console.log('blah', blah);
+    $(cardClicked).addClass('flipped');
+}
